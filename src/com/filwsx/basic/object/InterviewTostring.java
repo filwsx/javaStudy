bea5523ba60260d0c0512b6240a11ec5d4321de8
filwsx <endwoggg@gmail.com>
2022-01-21 18:50:33 +0800

    Using JUnit5 and Creating InterviewTostring

---
+++ src/com/filwsx/basic/object/InterviewTostring.java
@@ -0,0 +1,15 @@
+package com.filwsx.basic.object;
+
+import org.junit.jupiter.api.Test;
+
+public class InterviewTostring {
+	@Test
+	public void test() {
+		char[] arr = new char[] { 'a', 'b', 'c' };
+		System.out.println(arr);//
+		int[] arr1 = new int[] { 1, 2, 3 };
+		System.out.println(arr1);//
+		double[] arr2 = new double[] { 1.1, 2.2, 3.3 };
+		System.out.println(arr2);//
+	}
+}
