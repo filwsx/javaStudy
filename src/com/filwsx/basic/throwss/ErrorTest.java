1df0a2666071b084dcee5f77e0dc614bb8d3557e
filwsx <endwoggg@gmail.com>
2022-01-28 15:12:12 +0800

    Error Test

---
+++ src/com/filwsx/basic/throwss/ErrorTest.java
@@ -0,0 +1,11 @@
+package com.filwsx.basic.throwss;
+
+public class ErrorTest {
+	public static void main(String args[]) {
+		//StackOverflowError
+		//main(args);
+		
+		//OOM:OutOfMemoryError
+		//Integer arr[] = new Integer[1024*1024*1024];
+	}
+}
