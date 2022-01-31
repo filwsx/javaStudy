4d6e65a375e36e96a99306cb12645f644fa14d6b
filwsx <endwoggg@gmail.com>
2022-01-31 13:38:54 +0800

    除夕快乐！祝大家新的一年稳稳当当，不再变差！

---
+++ src/com/filwsx/basic/HelloJava/happySpringFestivalsEve.java
@@ -0,0 +1,10 @@
+package com.filwsx.basic.HelloJava;
+
+public class happySpringFestivalsEve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Happy Spring Festival's Eve");
+	}
+
+}
