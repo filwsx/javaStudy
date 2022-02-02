fd3a7392dec98086df8b3443d03c8cfdaaa5c570
filwsx <endwoggg@gmail.com>
2022-02-02 17:32:35 +0800

    update deadLock

---
+++ JavaAdvanced/src/com/filwsx/thread/DeadLockTest.java
@@ -0,0 +1,56 @@
+package com.filwsx.thread;
+
+/**
+ * @author filwsx
+ * @date 20220202 17:19
+ */
+public class DeadLockTest {
+    public static void main(String[] args) {
+
+        StringBuffer s1 = new StringBuffer();
+        StringBuffer s2 = new StringBuffer();
+
+        new Thread(){
+            @Override
+            public void run() {
+                synchronized (s1){
+                    s1.append("a");
+                    s2.append("1");
+                    try {
+                        Thread.sleep(100);  //增加死锁概率
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (s2){
+                        s1.append("b");
+                        s2.append("2");
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (s2){
+                    s1.append("c");
+                    s2.append("3");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (s1){
+                        s1.append("d");
+                        s2.append("4");
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }).start();
+
+    }
+}
--- JavaAdvanced/src/com/filwsx/thread/SynchronizedTest.java
+++ JavaAdvanced/src/com/filwsx/thread/SynchronizedTest.java
@@ -8,6 +8,10 @@ import static java.lang.Thread.sleep;
  */
 public class SynchronizedTest {
     public static void main(String[] args) {
+        TestSellerBetter();
+        TestSellerBetter2();
+    }
+    public static void TestSellerBetter(){
         SellerBetter ss1 = new SellerBetter();
         Thread tt1 = new Thread(ss1);
         Thread tt2 = new Thread(ss1);
@@ -15,6 +19,19 @@ public class SynchronizedTest {
         tt1.setName("2号窗口");
         tt2.setName("1号窗口");
         tt3.setName("3号窗口");
+        tt1.start();
+        tt2.start();
+        tt3.start();
+    }
+
+    public static void TestSellerBetter2(){
+        SellerBetter2 s1 = new SellerBetter2();
+        Thread tt1 = new Thread(s1);
+        Thread tt2 = new Thread(s1);
+        Thread tt3 = new Thread(s1);
+        tt1.setName("窗口1号");
+        tt2.setName("窗口2号");
+        tt3.setName("窗口3号");
         tt1.start();
         tt2.start();
         tt3.start();
@@ -40,6 +57,27 @@ class SellerBetter implements Runnable{
                     break;
                 }
             }
+        }
+    }
+}
+
+class SellerBetter2 implements Runnable{
+    private int ticket = 1000;   //因为只需要创建一个对象，传入到多个线程对象之中，所以并不需要static修饰
+    Object obj = new Object();  //任何一个对象都可充当锁，this关键字用自己也可，只要唯一
+    @Override
+    public void run() {
+        while(true){
+            sellTicket();
+            if(ticket == 0){
+                break;
+            }
+        }
+    }
+    public synchronized void sellTicket(){//同步监视器为当前类的对象
+        //while不能写在这个方法里，否则永远就是这个方法在运行了！！！
+        if (ticket > 0) {
+            System.out.println(Thread.currentThread().getName() + "卖的，票号为：" + ticket);
+            ticket--;
         }
     }
 }
