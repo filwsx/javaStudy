e8db58ac6587805d1d265a415172b6fee4c9075a
filwsx <endwoggg@gmail.com>
2022-02-02 22:34:09 +0800

    add ThreadPoolTest

---
+++ JavaAdvanced/src/com/filwsx/thread/CallableTest.java
@@ -0,0 +1,39 @@
+package com.filwsx.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author filwsx
+ * @date 2022-02-02 21:49
+ */
+public class CallableTest implements Callable {
+    @Override
+    public Object call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (i%2==0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
+class CallableThread{
+    public static void main(String[] args) {
+        CallableTest ct = new CallableTest();
+        FutureTask ft = new FutureTask(ct);
+        new Thread(ft).start();
+        Object sum = null;
+        try {
+            sum = ft.get();
+            System.out.println(sum);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ JavaAdvanced/src/com/filwsx/thread/ThreadPoolTest.java
@@ -0,0 +1,43 @@
+package com.filwsx.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author filwsx
+ * @date 2022-02-02 22:22
+ */
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        //4.设置线程池的属性
+        //System.out.println(service.getClass());
+        ThreadPoolExecutor service1 = (ThreadPoolExecutor) service;
+        //service1.setCorePoolSize(15);
+        //service1.setKeepAliveTime();
+
+        //2.执行指定的线程的操作。需要提供实现Runnable接口或Callable接口实现类的对象
+        service.execute(new Thread1());
+        service.execute(new Thread2());
+        //service.submit(Callable callable);//适合使用于Callable
+
+        //3.关闭连接池
+        service.shutdown();
+    }
+}
+
+class Thread1 implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
+
+class Thread2 implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
