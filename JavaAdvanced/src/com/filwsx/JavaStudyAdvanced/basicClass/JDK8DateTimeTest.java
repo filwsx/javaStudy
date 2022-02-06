eee73ac11997fdfaa475dbcc49331067d0e37924
filwsx <endwoggg@gmail.com>
2022-02-06 22:10:02 +0800

    update API的学习跳着来，看代码，这个没啥技术含量得多谢多看文档

---
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/basicClass/JDK8DateTimeTest.java
@@ -0,0 +1,109 @@
+package com.filwsx.JavaStudyAdvanced.basicClass;
+
+import org.junit.Test;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
+
+/**
+ * @author filwsx
+ * @date 2022-02-06 20:52
+ */
+public class JDK8DateTimeTest {
+    public static void main(String[] args) {
+        //Date date = new Date(2022 - 1900,2 - 1,8);
+        //System.out.println(date);
+    }
+
+    @Test
+    public void test3(){
+        //方式一：预定义的标准格式。如：ISO_LOCAL_DATE_TIME;ISO_LOCAL_DATE;ISO_LOCAL_TIME
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        //格式化:日期-->字符串
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String str1 = formatter.format(localDateTime);
+        //解析：字符串 -->日期
+        TemporalAccessor parse = formatter.parse("2022-02-06T22:03:50.043");
+
+        //方式二：
+        //本地化相关的格式。如：ofLocalizedDateTime()
+        //FormatStyle.LONG / FormatStyle.MEDIUM / FormatStyle.SHORT :适用于LocalDateTime
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
+        //格式化
+        String str2 = formatter1.format(localDateTime);
+
+        //本地化相关的格式。如：ofLocalizedDate()
+        //FormatStyle.FULL / FormatStyle.LONG / FormatStyle.MEDIUM / FormatStyle.SHORT : 适用于LocalDate
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+        //格式化
+        String str3 = formatter2.format(LocalDate.now());
+
+        //【重点】方式三：自定义的格式。如：ofPattern(“yyyy-MM-dd hh:mm:ss”)
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        //格式化
+        String str4 = formatter3.format(LocalDateTime.now());
+        //解析
+        TemporalAccessor accessor = formatter3.parse("2022-02-06 10:04:26");
+    }
+
+    @Test
+    public void instantTest(){
+        //now():获取本初子午线对应的标准时间
+        Instant instant = Instant.now();
+        System.out.println(instant);//2022-02-06T13:28:26.424Z
+
+        //添加时间的偏移量
+        OffsetDateTime offsetDateTime = instant.atOffset(ZoneOffset.ofHours(8));
+        System.out.println(offsetDateTime);//2022-02-06T21:28:26.424+08:00
+
+        //toEpochMilli():获取自1970年1月1日0时0分0秒（UTC）开始的毫秒数  ---> Date类的getTime()
+        long milli = instant.toEpochMilli();
+        System.out.println(milli);
+
+        //ofEpochMilli():通过给定的毫秒数，获取Instant实例  -->Date(long millis)
+        Instant instant1 = Instant.ofEpochMilli(1644154106424L);
+        System.out.println(instant1);
+    }
+
+    @Test
+    public void LocalDateTimeTest(){
+        //now()
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDate);
+        System.out.println(localTime);
+        System.out.println(localDateTime);
+
+        //of()
+        LocalDateTime ldt = LocalDateTime.of(2022,2,6,21,13,55);
+        System.out.println(ldt);
+
+        //get()
+        System.out.println(localDateTime.getDayOfMonth());
+        System.out.println(localDateTime.getDayOfWeek());
+        System.out.println(localDateTime.getMonth());
+        System.out.println(localDateTime.getMonthValue());
+        System.out.println(localDateTime.getMinute());
+
+        //withXxx():设置相关的属性
+        LocalDate localDate1 = localDate.withDayOfMonth(22);
+        System.out.println(localDate);
+        System.out.println(localDate1);
+
+        LocalDateTime localDateTime2 = localDateTime.withHour(4);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime2);
+
+        //不可变性
+        LocalDateTime localDateTime3 = localDateTime.plusMonths(3);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime3);
+
+        LocalDateTime localDateTime4 = localDateTime.minusDays(6);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime4);
+    }
+}
