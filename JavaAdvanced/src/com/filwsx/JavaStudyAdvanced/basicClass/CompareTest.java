79d4944731f438c7df0a7da4879bd1a0a5ef2f4a
filwsx <endwoggg@gmail.com>
2022-02-07 00:55:09 +0800

    update System、Math、Big*

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/basicClass/CompareTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/basicClass/CompareTest.java
@@ -3,6 +3,7 @@ package com.filwsx.JavaStudyAdvanced.basicClass;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author filwsx
@@ -11,8 +12,67 @@ import java.util.Arrays;
 public class CompareTest {
 
     @Test
-    public void test2(){
+    public void test4(){
+        Goods[] arr = new Goods[6];
+        arr[0] = new Goods("lenovoMouse",34);
+        arr[1] = new Goods("dellMouse",43);
+        arr[2] = new Goods("xiaomiMouse",12);
+        arr[3] = new Goods("huaweiMouse",65);
+        arr[4] = new Goods("huaweiMouse",224);
+        arr[5] = new Goods("microsoftMouse",43);
 
+        Arrays.sort(arr, new Comparator() {
+            //指明商品比较大小的方式:按照产品名称从低到高排序,再按照价格从高到低排序
+            @Override
+            public int compare(Object o1, Object o2) {
+                if(o1 instanceof Goods && o2 instanceof Goods){
+                    Goods g1 = (Goods)o1;
+                    Goods g2 = (Goods)o2;
+                    if(g1.getName().equals(g2.getName())){
+                        return -Double.compare(g1.getPrice(),g2.getPrice());
+                    }else{
+                        return g1.getName().compareTo(g2.getName());
+                    }
+                }
+                throw new RuntimeException("输入的数据类型不一致");
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void test3(){
+        String[] arr = new String[]{"AA","CC","KK","MM","GG","JJ","DD"};
+        Arrays.sort(arr,new Comparator(){
+            //按照字符串从大到小的顺序排列
+            @Override
+            public int compare(Object o1, Object o2) {
+                if(o1 instanceof String && o2 instanceof  String){
+                    String s1 = (String) o1;
+                    String s2 = (String) o2;
+                    return -s1.compareTo(s2);   //改为从大到小
+                }
+//                return 0;
+                throw new RuntimeException("输入的数据类型不一致");
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void test2(){
+        Goods[] arr = new Goods[5];
+        arr[0] = new Goods("lenovoMouse",34);
+        arr[1] = new Goods("dellMouse",43);
+        arr[2] = new Goods("xiaomiMouse",12);
+        arr[3] = new Goods("huaweiMouse",65);
+        arr[4] = new Goods("microsoftMouse",43);
+
+        Arrays.sort(arr);
+        // 因为对象无法比较大小，但是实际又有需求，我们通过写一个方法，告诉sort方法如何比较两个同类对象比大小
+        // sort实现算法其他部分并不受影响
+
+        System.out.println(Arrays.toString(arr));
     }
 
     @Test
@@ -73,11 +133,11 @@ class Goods implements Comparable{
             }else if(this.price < goods.price){
                 return -1;
             }else{
-//                return 0;
-                return -this.name.compareTo(goods.name);
+                //return 0;
+                return -this.name.compareTo(goods.name);    //调用String的方法
             }
             //方式二：
-//           return Double.compare(this.price,goods.price);
+            //return Double.compare(this.price,goods.price);
         }
         throw new RuntimeException("传入的数据类型不一致！");
     }
