ba5516346ad602e43e2a139b9d7f7c5afd8c52c9
filwsx <endwoggg@gmail.com>
2022-02-07 15:39:00 +0800

    update CollectionTest

---
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/collection/CollectionTest.java
@@ -0,0 +1,43 @@
+package com.filwsx.JavaStudyAdvanced.collection;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author filwsx
+ * @date 2022-02-07 15:36
+ */
+public class CollectionTest {
+    @Test
+    public void test1(){
+        Collection coll = new ArrayList();
+
+        //add(Object e):将元素e添加到集合coll中
+        coll.add("Wu");
+        coll.add("Long");
+        coll.add("Fei");
+        coll.add(123);//自动装箱
+        coll.add(LocalDate.now());
+
+        //size():获取添加的元素的个数
+        System.out.println(coll.size());//4
+
+        //addAll(Collection coll1):将coll1集合中的元素添加到当前的集合中
+        Collection coll1 = new ArrayList();
+        coll1.add(456);
+        coll1.add("CC");
+        coll.addAll(coll1);
+
+        System.out.println(coll.size());//6
+        System.out.println(coll);
+
+        //clear():清空集合元素
+        coll.clear();
+
+        //isEmpty():判断当前集合是否为空
+        System.out.println(coll.isEmpty());
+    }
+}
