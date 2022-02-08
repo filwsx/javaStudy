e142dba3a9278e01cd5b531dc9922a0f08f99a60
filwsx <endwoggg@gmail.com>
2022-02-08 12:34:21 +0800

    add IteratorTest.java

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/collection/CollectionTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/collection/CollectionTest.java
@@ -5,13 +5,98 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author filwsx
  * @date 2022-02-07 15:36
  */
 public class CollectionTest {
+
+    @Test
+    public void test5(){
+        Collection coll = new ArrayList();
+        coll.addAll(Arrays.asList(456,123,new Person("Jerry",20),new String("Tom"),false));
+
+        //7.hashCode():返回当前对象的哈希值
+        System.out.println(coll.hashCode());
+
+        //8.集合 --->数组：toArray()
+        Object[] arr = coll.toArray();
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        //拓展：数组 --->集合:调用Arrays类的静态方法asList()
+        List<String> list = Arrays.asList(new String[]{"AA", "BB", "CC"});
+        System.out.println(list);
+
+        //以下两个例子，存储内容看似一样，但长度不一致
+        List arr1 = Arrays.asList(new int[]{123, 456});
+        List arr2 = Arrays.asList(123, 456);
+        System.out.println(arr1.size());//1
+        System.out.println(arr2.size());//2
+        List arr3 = Arrays.asList(new Integer[]{123, 456});
+        System.out.println(arr3.size());//2
+
+    }
+
+    @Test
+    public void test4(){
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+
+        //5.retainAll(Collection coll1):交集：获取当前集合和coll1集合的交集，并返回给当前集合
+        Collection coll1 = new ArrayList();
+        coll1.addAll(Arrays.asList(123,789,456));
+        coll1.retainAll(coll);
+        System.out.println(coll1);
+
+        //6.equals(Object obj):要想返回true，需要当前集合和形参集合的元素都相同。
+        coll1.clear();
+        coll1.addAll(Arrays.asList(456,123,new Person("Jerry",20),new String("Tom"),false));
+        System.out.println(coll.equals(coll1));
+        System.out.println(coll);
+        System.out.println(coll1);
+        coll1.clear();
+        coll1.addAll(Arrays.asList(123,456,new Person("Jerry",20),new String("Tom"),false));
+        System.out.println(coll.equals(coll1));
+        System.out.println(coll);
+        System.out.println(coll1);
+        //对比发现，这种对比要求元素顺序一致 20220208 1151
+    }
+
+    @Test
+    public void test3(){
+        //3.remove(Object obj):从当前集合中移除obj元素。
+        Collection coll = new ArrayList();
+        coll.add(123);
+        coll.add(456);
+        coll.add(new Person("Jerry",20));
+        coll.add(new String("Tom"));
+        coll.add(false);
+        System.out.println(coll);
+
+        coll.remove(1234);
+        System.out.println(coll);
+        coll.remove(123);
+        System.out.println(coll);
+        coll.remove(new Person("Jerry",20));
+        System.out.println(coll);
+
+        //4. removeAll(Collection coll1):差集：从当前集合中移除coll1中所有的元素。
+        Collection coll1 = Arrays.asList(123,456);
+        coll.removeAll(coll1);
+        System.out.println(coll);
+
+    }
+
     @Test
     public void test2(){
         Collection coll = new ArrayList();
@@ -29,6 +114,8 @@ public class CollectionTest {
         contains = coll.contains(new Person("me",35));
         System.out.println(contains);
 
+        Collection coll1 = Arrays.asList(123,4567);
+        System.out.println(coll.containsAll(coll1));
     }
 
     @Test
---
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/collection/IteratorTest.java
@@ -0,0 +1,56 @@
+package com.filwsx.JavaStudyAdvanced.collection;
+
+import com.filwsx.JavaStudyAdvanced.Annotation.Person;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * @author filwsx
+ * @date 2022-02-08 12:08
+ */
+public class IteratorTest {
+
+    // 测试Iterator中的remove()
+    // 在next()方法前调用remove()、在next()后调用两次remove()，都会产生IllegalStateException。
+    @Test
+    public void test2(){
+        Collection coll = new ArrayList();
+        coll.addAll(Arrays.asList(123,456,new Person("Jerry",20),new String("Tom"),false));
+        Iterator iterator = coll.iterator();
+
+        //删除集合中"Tom"
+        while (iterator.hasNext()){
+            // iterator.remove();        // 此语句执行会报异常
+            Object obj = iterator.next();
+            if("Tom".equals(obj)){
+                iterator.remove();
+                // iterator.remove();    // 此语句执行会报异常
+            }
+        }
+    }
+
+    @Test
+    public void test1(){
+        Collection coll = new ArrayList();
+        coll.addAll(Arrays.asList(123,456,new Person("Jerry",20),new String("Tom"),false));
+        Iterator iterator = coll.iterator();
+
+        //不推荐for结合获取coll的size来遍历，要hasNext用起来！
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        // 错误实现1：
+        // while((iterator.next()) != null){System.out.println(iterator.next());}
+
+        // 错误实现2：
+        // 集合对象每次调用iterator()方法都得到一个全新的迭代器对象，默认游标都在集合的第一个元素之前。
+        while (coll.iterator().hasNext()){
+            System.out.println(coll.iterator().next());
+        }
+    }
+}
