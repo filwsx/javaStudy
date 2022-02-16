54a67c0e5d1d89601ac61620ac6fb233ea1e8f29
filwsx <endwoggg@gmail.com>
2022-02-16 10:27:14 +0800

    update file

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/file/FileTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/file/FileTest.java
@@ -3,6 +3,8 @@ package com.filwsx.JavaStudyAdvanced.file;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * @author filwsx
@@ -10,7 +12,103 @@ import java.io.File;
  */
 public class FileTest {
 
+    @Test
+    public void test7(){
+        //文件目录的创建
+        File file1 = new File("C:\\Users\\filwsx\\Documents\\test");
 
+        boolean mkdir = file1.mkdir();
+        if(mkdir){
+            System.out.println("创建成功1");
+        }
+
+        System.out.println(file1.delete());
+    }
+
+    @Test
+    public void test6() throws IOException {
+        File file1 = new File("hi.txt");
+        if(!file1.exists()){
+            //文件的创建
+            file1.createNewFile();
+            System.out.println("创建成功");
+        }else{//文件存在
+            file1.delete();
+            System.out.println("删除成功");
+        }
+    }
+
+    @Test
+    public void test5(){
+        File file1 = new File("hello2.txt");
+
+        System.out.println(file1.isDirectory());
+        System.out.println(file1.isFile());
+        System.out.println(file1.exists());
+        System.out.println(file1.canRead());
+        System.out.println(file1.canWrite());
+        System.out.println(file1.isHidden());
+
+        System.out.println();
+
+        File file2 = new File("C:\\Users\\");
+        System.out.println(file2.isDirectory());
+        System.out.println(file2.isFile());
+        System.out.println(file2.exists());
+        System.out.println(file2.canRead());
+        System.out.println(file2.canWrite());
+        System.out.println(file2.isHidden());
+    }
+
+    @Test
+    public void test4(){
+        File file1 = new File("hello.txt");
+        File file2 = new File("hello2.txt");
+
+        boolean renameTo = file1.renameTo(file2);
+        System.out.println(renameTo);
+
+    }
+
+    @Test
+    public void test3(){
+        File file = new File("C:\\Users\\");
+
+        String[] list = file.list();
+        for(String s : list){
+            System.out.println(s);
+        }
+
+        System.out.println();
+
+        File[] files = file.listFiles();
+        for(File f : files){
+            System.out.println(f);
+        }
+
+    }
+
+    @Test
+    public void test2(){
+        File file1 = new File("hello.txt");
+        File file2 = new File("C:\\Users\\hello.txt");
+
+        System.out.println(file1.getAbsolutePath());
+        System.out.println(file1.getPath());
+        System.out.println(file1.getName());
+        System.out.println(file1.getParent());
+        System.out.println(file1.length());
+        System.out.println(new Date(file1.lastModified()));
+
+        System.out.println();
+
+        System.out.println(file2.getAbsolutePath());
+        System.out.println(file2.getPath());
+        System.out.println(file2.getName());
+        System.out.println(file2.getParent());
+        System.out.println(file2.length());
+        System.out.println(file2.lastModified());
+    }
 
     @Test
     public void test1(){
