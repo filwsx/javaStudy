76a64804bdafe6742cc572fc7abe2b596766f4a7
filwsx <endwoggg@gmail.com>
2022-02-17 19:44:54 +0800

    update TCPtest

---
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NetWork/TCPtest.java
@@ -0,0 +1,109 @@
+package com.filwsx.JavaStudyAdvanced.NetWork;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 有计算机网络基础，已下载跳过好多课，有点爽！1929写
+ * @author filwsx
+ * @date 2022-02-17 19:27
+ */
+public class TCPtest {
+
+    @Test
+    public void client1() {
+        Socket socket =  null;
+        OutputStream os = null;
+        try {
+            InetAddress inet = InetAddress.getByName("127.0.0.1");
+            socket = new Socket(inet,8080);
+            os = socket.getOutputStream();
+            os.write("这是一条客户端请求".getBytes(StandardCharsets.UTF_8));
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(os != null){
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }if(socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void server1(){
+        ServerSocket ss = null;
+        Socket socket = null;
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            //1.创建服务器端的ServerSocket，指明自己的端口号
+            ss = new ServerSocket(8080);
+            //2.调用accept()表示接收来自于客户端的socket
+            socket = ss.accept();
+            //3.获取输入流
+            is = socket.getInputStream();
+
+            //4.读取输入流中的数据
+            baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[5];
+            int len;
+            while((len = is.read(buffer)) != -1){
+                baos.write(buffer,0,len);
+            }
+
+            System.out.println(baos.toString());
+            System.out.println("收到了来自于：" + socket.getInetAddress().getHostAddress() + "的数据");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(baos != null){
+                //5.关闭资源
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(ss != null){
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
---
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NetWork/UDPtest.java
@@ -0,0 +1,9 @@
+package com.filwsx.JavaStudyAdvanced.NetWork;
+
+/**
+ * @author filwsx
+ * @date 2022-02-17 19:28
+ */
+public class UDPtest {
+
+}
