2150019714de801b8c77fda58983d07381b92fd1
filwsx <endwoggg@gmail.com>
2022-02-17 23:11:14 +0800

    update url

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NetWork/URLtest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NetWork/URLtest.java
@@ -1,9 +1,71 @@
 package com.filwsx.JavaStudyAdvanced.NetWork;
 
+import org.junit.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author filwsx
  * @date 2022-02-17 19:28
  */
 public class URLtest {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://wx1.sinaimg.cn/large/006xTu1xly1gxxgm64053j32c02c0hdt.jpg");
+            System.out.println(url.getProtocol());
+            System.out.println(url.getHost());
+            System.out.println(url.getPort());
+            System.out.println(url.getPath());
+            System.out.println(url.getFile());
+            System.out.println(url.getQuery());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void test(){
+        HttpURLConnection urlConnection = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            URL url = new URL("https://wx1.sinaimg.cn/large/006xTu1xly1gxxgm64053j32c02c0hdt.jpg");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            is = urlConnection.getInputStream();
+            fos = new FileOutputStream("beauty.jpg");
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = is.read(buffer)) != -1){
+                fos.write(buffer,0,len);
+            }
+            System.out.println("下载完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            if(is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+    }
 }
