45f57331a4e490bdc8e82beb6fa16a4241976d13
filwsx <endwoggg@gmail.com>
2022-02-20 23:42:39 +0800

    update

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/ConstructorRefTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/ConstructorRefTest.java
@@ -3,6 +3,7 @@ package com.filwsx.JavaStudyAdvanced.NewFeature;
 import com.filwsx.JavaStudyAdvanced.Annotation.Person;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -12,10 +13,18 @@ import java.util.function.Supplier;
  */
 public class ConstructorRefTest {
 
-    //
+    //数组引用 Function中的R apply(T t)
     @Test
     public void test3(){
+        Function<Integer,String[]> func1 = length -> new String[length];
+        String[] arr1 = func1.apply(5);
+        System.out.println(Arrays.toString(arr1));
 
+        System.out.println("*******************");
+
+        Function<Integer,String[]> func2 = String[] :: new;
+        String[] arr2 = func2.apply(10);
+        System.out.println(Arrays.toString(arr2));
     }
 
     //Function中的R apply(T t)
@@ -27,7 +36,8 @@ public class ConstructorRefTest {
         fuc2 = Person::new;
         MyInterface<String,Integer,Person> fuc3 = (name,age) -> new Person(name,age);
         fuc3 = Person::new;
-        //经过此测试，我理解了lambda了!!! 还用上了自定义函数式接口和泛型 20220220 2329
+        //经过fuc3测试，我理解了lambda了!!! 还用上了自定义函数式接口和泛型 这是自己冒出的想法20220220 2329
+        //2340发现课程里的test3()貌似就是我fuc3的想法！
 
         System.out.println(fuc1.apply(13));
         System.out.println(fuc2.apply("Tom"));
