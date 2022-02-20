723127361cf9c727e88cc5fde917651e254ad692
filwsx <endwoggg@gmail.com>
2022-02-20 22:46:10 +0800

    lambda真好用！

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/MethodRefTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/MethodRefTest.java
@@ -3,7 +3,9 @@ package com.filwsx.JavaStudyAdvanced.NewFeature;
 import com.filwsx.JavaStudyAdvanced.Annotation.Person;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -11,7 +13,35 @@ import java.util.function.Supplier;
  * @date 2022-02-20 22:15
  */
 public class MethodRefTest {
-    // 情况一：对象 :: 实例方法
+    // 情况3：类::实例方法名
+    // Comparator中的int comapre(T t1,T t2) 与 String中的int t1.compareTo(t2)
+    // Function中的R apply(T t) 与 Person中的String getName();
+    @Test
+    public void test3() {
+        Comparator<String> con1 = String :: compareTo;
+        Function<Person,String> con2 = Person ::getName;
+        System.out.println(con2.apply(new Person("Tom", 13)));
+        System.out.println(con1.compare("abc", "abd"));
+    }
+
+    // 情况2：类::静态方法名
+    //Comparator中的int compare(T t1,T t2) 与 Integer中的int compare(T t1,T t2)
+    //Function中的R apply(T t) 与 Math中的Long round(Double d)
+    @Test
+    public void test2() {
+        Comparator<Integer> con1 = (t1,t2) -> Integer.compare(t1,t2);
+        System.out.println(con1.compare(12, 34));
+        Comparator<Integer> con2 = Integer::compare;
+        System.out.println(con2.compare(89, 64));
+
+        Function<Double,Long> con3 = d -> Math.round(d);
+        System.out.println(con3.apply(1.4));
+        Function<Double,Long> con4 = Math::round;
+        System.out.println(con4.apply(2.9));
+
+    }
+
+    // 情况1：对象 :: 实例方法
     // Consumer中的void accept(T t) 与 PrintStream中的void println(T t)
     @Test
     public void test1() {
