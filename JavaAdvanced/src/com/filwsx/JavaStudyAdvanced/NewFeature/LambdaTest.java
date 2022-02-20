d57a70742f5399ad3623ef5bb0246a6286f3ee31
filwsx <endwoggg@gmail.com>
2022-02-20 21:29:42 +0800

    bad code

--- JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/LambdaTest.java
+++ JavaAdvanced/src/com/filwsx/JavaStudyAdvanced/NewFeature/LambdaTest.java
@@ -2,13 +2,49 @@ package com.filwsx.JavaStudyAdvanced.NewFeature;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * @author filwsx
  * @date 2022-02-19 18:40
  */
 public class LambdaTest {
+
+    //Consumer函数式
+    //看完代码后，根据理解自己的写的！ 20220220 2115
+    @Test
+    public void test4(){
+        List<String> list = Arrays.asList("北京","南京","天津","东京","西京","普京");
+        //List<String> res = filterString(list,(s,c) -> s.contains((String)c)); //这样写不行
+        List<String> res = filterString(list,s -> s.contains("京"));
+        System.out.println(res);
+    }
+
+    public List<String> filterString(List<String> list , Predicate<String> pre){
+        List<String> res = Arrays.asList();
+        for(String s:list){
+            if(pre.test(s)){
+                res.add(s);
+            }
+        }
+        return res;
+    }
+
+    //Consumer函数式
+    //看完代码后，根据理解，自己的写的！ 20220220 2115
+    @Test
+    public void test3(){
+        badTime(520,money2 -> System.out.println("送女朋友礼物花了"+money2+"元"));
+        badTime(250,money2 -> System.out.println("刚送完礼物，分手吃饭花了"+money2+"元"));
+    }
+
+    public void badTime(double money , Consumer<Double> consumer){
+        consumer.accept(money);
+    }
 
     @Test
     public void test2(){
