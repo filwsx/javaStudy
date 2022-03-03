98ce971b4e5022923945c7f6d1e9fb6a629d224f
filwsx <endwoggg@gmail.com>
2022-03-03 12:54:13 +0800

    chapter10

--- Mysql/Day08_chapter10_note/Day08_chapter10_lecture.sql
+++ Mysql/Day08_chapter10_note/Day08_chapter10_lecture.sql
@@ -1,2 +1,27 @@
+#Chapter10_Create DataBase and manager it
+
 USE filwsxdb;
+
+CREATE DATABASE IF NOT EXISTS mytest CHARACTER SET 'GBK';
+
+SHOW TABLES;
+
+DROP DATABASE IF EXISTS mytest;
+
+USE mytest;
+
+SHOW CREATE DATABASE mytest;
+
+CREATE TABLE IF NOT EXISTS myempl(
+id INT,
+emp_name VARCHAR(15),
+hire_date DATE
+);
+
+#创建表的方式二:富二代
+CREATE TABLE myemp2
+AS
+#AS 后跟select语句，相当于复制表了
+#若使用别名，复制的表字段为别名
+SELECT * FROM filwsxdb.`employees`;
 
