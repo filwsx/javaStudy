679182f90950ab548229411ed91f61f797886de6
filwsx <endwoggg@gmail.com>
2022-03-03 15:43:35 +0800

    mysql chapter10 exercise2-3 init

---
+++ Mysql/Day08_chapter10_note/Day08_chapter10_exercise2.sql
@@ -0,0 +1,15 @@
+# 1、创建数据库 test02_market
+
+# 2、创建数据表 customers
+
+# 3、将 c_contact 字段移动到 c_birth 字段后面
+
+# 4、将 c_name 字段数据类型改为 varchar(70)
+
+# 5、将c_contact字段改名为c_phone
+
+# 6、增加c_gender字段到c_name后面，数据类型为char(1)
+
+# 7、将表名改为customers_info
+
+# 8、删除字段c_city
---
+++ Mysql/Day08_chapter10_note/Day08_chapter10_exercise3.sql
@@ -0,0 +1,17 @@
+# 1、创建数据库test03_company
+
+# 2、创建表offices
+
+# 3、创建表employees
+
+# 4、将表employees的mobile字段修改到code字段后面
+
+# 5、将表employees的birth字段改名为birthday
+
+# 6、修改sex字段，数据类型为char(1)
+
+# 7、删除字段note
+
+# 8、增加字段名favoriate_activity，数据类型为varchar(100)
+
+# 9、将表employees的名称修改为 employees_info
