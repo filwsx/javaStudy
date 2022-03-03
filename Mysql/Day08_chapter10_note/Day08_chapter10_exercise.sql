03c19df379bc4c5f3ad288ae45fc8293a55d6303
filwsx <endwoggg@gmail.com>
2022-03-03 12:08:28 +0800

    chapter10 init

---
+++ Mysql/Day08_chapter10_note/Day08_chapter10_exercise.sql
@@ -0,0 +1,33 @@
+USE filwsxdb;
+
+#1.创建数据库test01_office,指明字符集为utf8。并在此数据库下执行下述操作
+
+#2.创建表dept01
+/*
+字段 类型
+id INT(7)
+NAME VARCHAR(25)
+*/
+
+#3.将表departments中的数据插入新表dept02中
+
+#4.创建表emp01
+/*
+字段 类型
+id INT(7)
+first_name VARCHAR (25)
+last_name VARCHAR(25)
+dept_id INT(7)
+*/
+
+#5.将列last_name的长度增加到50
+
+#6.根据表employees创建emp02
+
+#7.删除表emp01
+
+#8.将表emp02重命名为emp01
+
+#9.在表dept02和emp01中添加新列test_column，并检查所作的操作
+
+#10.直接删除表emp01中的列 department_id
