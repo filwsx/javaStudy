2edc43d4011f04d7eb8309db4ca181cf0fdde884
filwsx <endwoggg@gmail.com>
2022-03-03 14:38:12 +0800

    mysql

--- Mysql/Day08_chapter10_note/Day08_chapter10_lecture.sql
+++ Mysql/Day08_chapter10_note/Day08_chapter10_lecture.sql
@@ -39,17 +39,28 @@ ALTER TABLE myempl
 ADD phone_number VARCHAR(20) AFTER emp_name;
 
 #3.2 修改一个字段：数据类型、长度、默认值（略）
+ALTER TABLE myempl
+MODIFY phone_number INT DEFAULT 110;
 
 #3.3 重命名一个字段
+ALTER TABLE myemp2
+CHANGE anu_salary mon_salary DOUBLE(10,2);
 
 #3.4 删除一个字段
+ALTER TABLE myempl
+DROP COLUMN id;
 
 #管理表
 #4.1 重命名表
+RENAME TABLE myempl
+TO myemp1;	#数字1 和 字母L
 
 #4.2 删除表
+DROP TABLE IF EXISTS myempl;	#从删库到跑路，无法回滚
 
-#4.3 清空表
+#4.3 清空表-保留表结构，不要数据
+SELECT * FROM myemp11;
+TRUNCATE TABLE myemp11;
 
 #5. DCL 中 COMMIT 和 ROLLBACK
 
