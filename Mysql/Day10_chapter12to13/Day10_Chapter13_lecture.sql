8e14df670cbed49c753796d72c41978825e144f9
filwsx <endwoggg@gmail.com>
2022-03-05 15:06:23 +0800

    chapter 13 lecture 主键与外键为同一列

--- Mysql/Day10_chapter12to13/Day10_Chapter13_lecture.sql
+++ Mysql/Day10_chapter12to13/Day10_Chapter13_lecture.sql
@@ -46,3 +46,41 @@ INSERT INTO `user_info`() VALUES
 
 #问题2：子表可以有几个外键
 #这个有答案，我没仔细听。可以有多个外键约束。
+
+#子表外键和子表主键在同一列，可否？
+CREATE TABLE user2(
+id INT,
+`NAME` VARCHAR(15),
+`PASSWORD` VARCHAR(25),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE user_info2(
+id INT,
+addr VARCHAR(50),
+PRIMARY KEY (id),
+CONSTRAINT fk_ui2_id FOREIGN KEY (id) REFERENCES user2(id)
+);
+
+INSERT INTO user2(id,`name`,`password`) VALUES 
+(1,'Tom','123'),
+(2,'Tom1','123'),
+(3,'Tom','abc');
+
+INSERT INTO user_info2(id,addr) VALUES 
+(1,'NY'),
+(2,'LA'),
+(3,'湾区');
+
+#失败！
+INSERT INTO user_info2(id,addr) VALUES 
+(0,'NY');
+
+INSERT INTO user2(id,`name`,`password`) VALUES 
+(0,'Tom','1230');
+
+#成功
+INSERT INTO user_info2(id,addr) VALUES 
+(0,'NY');
+#20220305 1505完成测试验证
+DESC user_info2;
