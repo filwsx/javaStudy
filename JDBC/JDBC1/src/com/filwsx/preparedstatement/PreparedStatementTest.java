41a0d7a902752e9be7976bb40736a1c4cbeca287
filwsx <endwoggg@gmail.com>
2022-03-06 19:22:54 +0800

    PreparedStatement insert test

---
+++ JDBC/JDBC1/src/com/filwsx/preparedstatement/PreparedStatementTest.java
@@ -0,0 +1,70 @@
+package com.filwsx.preparedstatement;
+
+import org.junit.Test;
+
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
+/**
+ * @author filwsx
+ * @date 2022-03-06 18:00;
+ */
+public class PreparedStatementTest {
+
+    @Test
+    public void upateTest() throws Exception {
+        Connection con = null;
+        PreparedStatement ps = null;
+        //无法使用Java8新特性，try()
+        try{
+            Properties pro = new Properties();
+            // 这么写还得关掉Stream，麻烦！
+            // FileInputStream  fis = new FileInputStream("jdbc.properties");
+            InputStream is = PreparedStatementTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            // String类不行
+            // InputStream is = String.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            pro.load(is);
+
+            String user = pro.getProperty("user");
+            String pwd = pro.getProperty("password");
+            String driverClass = pro.getProperty("driverClass");
+            String url = pro.getProperty("url");
+
+            Class.forName(driverClass);
+
+            con = DriverManager.getConnection(url,user,pwd);
+
+            String sql = "insert into customers(name,email,birth)values(?,?,?)";
+
+            ps = con.prepareStatement(sql);
+            ps.setString(1,"filwsx");
+            ps.setString(2,"filwsx@gmail.com");
+            // 字符串不行
+            // ps.setString(3,"1987-01-13");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            java.util.Date date = sdf.parse("1987-01-13");
+            Date sqlDate = new Date(date.getTime());
+            ps.setDate(3, sqlDate);
+            ps.execute();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if(ps != null)
+                    ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(con != null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
