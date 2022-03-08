704af8ef2e718c3e3bf275074318b32beb8b3f9d
filwsx <endwoggg@gmail.com>
2022-03-08 12:46:17 +0800

    exercise2

---
+++ JDBC/JDBC1/src/com/filwsx/exercise/exercise2.java
@@ -0,0 +1,114 @@
+package com.filwsx.exercise;
+
+import com.filwsx.bean.Student;
+import com.filwsx.utils.JDBCutils;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author filwsx
+ * @date 2022-03-08 11:39
+ */
+public class exercise2 {
+
+    @Test
+    public static void main(String[] args){
+        System.out.println("请输入学生的考号：");
+        Scanner scanner = new Scanner(System.in);
+        String examCard = scanner.next();
+        String sql = "delete from examstudent where examCard = ?";
+        int deleteCount = JDBCutils.insert(sql, examCard);
+        if(deleteCount > 0){
+            System.out.println("删除成功");
+        }else{
+            System.out.println("查无此人，请重新输入");
+        }
+    }
+
+    @Test
+    public static void main2(String[] args){
+        System.out.println("请选择您要输入的类型：");
+        System.out.println("a.准考证号");
+        System.out.println("b.身份证号");
+        Scanner scanner = new Scanner(System.in);
+        String selection = scanner.next();
+        if("a".equalsIgnoreCase(selection)){//if(selection.equalsIgnoreCase("a")){
+            System.out.println("请输入准考证号：");
+            String examCard = scanner.next();
+            String sql = "select FlowID flowID,Type type,IDCard,ExamCard examCard,StudentName name,Location location,Grade grade from examstudent where examCard = ?";
+
+            List<Student> list = JDBCutils.getList(Student.class,sql,examCard);
+            if(list.size() != 0){
+                list.forEach(System.out::println);
+            }else{
+                System.out.println("输入的准考证号有误！");
+            }
+
+        }else if("b".equalsIgnoreCase(selection)){
+            System.out.println("请输入身份证号：");
+            String IDCard = scanner.next();
+            String sql = "select FlowID flowID,Type type,IDCard,ExamCard examCard,StudentName name,Location location,Grade grade from examstudent where IDCard = ?";
+
+            List<Student> list = JDBCutils.getList(Student.class,sql,IDCard);
+            if(list.size() != 0){
+                list.forEach(System.out::println);
+            }else{
+                System.out.println("输入的身份证号有误！");
+            }
+        }else{
+            System.out.println("您的输入有误，请重新进入程序。");
+        }
+    }
+
+    @Test
+    public static void main1(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("四级/六级：");
+        int type = scanner.nextInt();
+        System.out.print("身份证号：");
+        String IDCard = scanner.next();
+        System.out.print("准考证号：");
+        String examCard = scanner.next();
+        System.out.print("学生姓名：");
+        String studentName = scanner.next();
+        System.out.print("所在城市：");
+        String location = scanner.next();
+        System.out.print("考试成绩：");
+        int grade = scanner.nextInt();
+
+        String sql = "insert into examstudent(type,IDCard,examCard,studentName,location,grade)values(?,?,?,?,?,?)";
+        int insertCount = JDBCutils.insert(sql,type,IDCard,examCard,studentName,location,grade);
+        if(insertCount > 0){
+            System.out.println("添加成功");
+        }else{
+            System.out.println("添加失败");
+        }
+    }
+
+    //构想：传入任意类对象组成的list，自动生成sql语句，批量插入
+    //不会写，发现自己对集合、泛型、反射掌握很差！！！ 20220308 1208 放弃
+    //public boolean insertByObject(List<T> list){ //参数不能这么写！
+    public boolean insertByObject(List list){
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = "insert into ";
+        try {
+            con = JDBCutils.getConnection();
+            ps = con.prepareStatement(sql);
+            for (int i = 0; i < 0; i++) {
+
+            }
+            return ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCutils.closeResoures(con,ps);
+        }
+        return false;
+    }
+}
--- JDBC/JDBC1/src/com/filwsx/utils/JDBCutils.java
+++ JDBC/JDBC1/src/com/filwsx/utils/JDBCutils.java
@@ -1,7 +1,10 @@
 package com.filwsx.utils;
 
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**操作数据库的工具类
@@ -9,6 +12,57 @@ import java.util.Properties;
  * @date 2022-03-07 15:06
  */
 public class JDBCutils {
+    public static <T> List<T> getList(Class<T> cla , String sql, Object ...args){
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<T> list = new ArrayList<T>();
+        try {
+            con = JDBCutils.getConnection();
+            ps = con.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1,args[i]);
+            }
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            while (rs.next()){
+                T t = cla.newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object value = rs.getObject(i+1);
+                    String columnName = rsmd.getColumnLabel(i+1);
+                    Field field = cla.getDeclaredField(columnName);
+                    field.setAccessible(true);
+                    field.set(t,value);
+                }
+                list.add(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCutils.closeResoures(con,ps,rs);
+        }
+        return list;
+    }
+
+    public static int insert(String sql,Object ... args){
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = JDBCutils.getConnection();
+            ps = con.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1,args[i]);
+            }
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCutils.closeResoures(con,ps);
+        }
+        return 0;
+    }
+
     public static Connection getConnection() throws Exception{
         InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
         Properties pro = new Properties();
