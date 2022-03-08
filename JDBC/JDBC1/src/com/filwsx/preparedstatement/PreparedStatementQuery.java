926dc1ca7337c9df8ad661f49ede8e5b5d54a033
filwsx <endwoggg@gmail.com>
2022-03-08 11:13:40 +0800

    update 通用查询不同表，返回多个结果

--- JDBC/JDBC1/src/com/filwsx/preparedstatement/PreparedStatementQuery.java
+++ JDBC/JDBC1/src/com/filwsx/preparedstatement/PreparedStatementQuery.java
@@ -10,12 +10,58 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author filwsx
  * @date 2022-03-08 10:56
  */
 public class PreparedStatementQuery {
+
+    @Test
+    public void testGetForList(){
+        String sql = "select id,name,email from customers where id < ?";
+        List<Customer> list = getList(Customer.class,sql,12);
+        list.forEach(System.out::println);
+
+        String sql1 = "select order_id orderId,order_name orderName from `order`";
+        List<Order> orderList = getList(Order.class, sql1);
+        orderList.forEach(System.out::println);
+    }
+
+    public <T> List<T> getList(Class<T> cla , String sql, Object ...args){
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<T> list = new ArrayList<T>();
+        try {
+            con = JDBCutils.getConnection();
+            ps = con.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1,args[i]);
+            }
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            while (rs.next()){
+                T t = cla.newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object value = rs.getObject(i+1);
+                    String columnName = rsmd.getColumnLabel(i+1);
+                    Field field = cla.getDeclaredField(columnName);
+                    field.setAccessible(true);
+                    field.set(t,value);
+                }
+                list.add(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCutils.closeResoures(con,ps,rs);
+        }
+        return list;
+    }
 
     @Test
     public void testGetInstance(){
