be727f08fefdd991cb1d119b5ab13ad92de43878
filwsx <endwoggg@gmail.com>
2022-04-02 13:00:08 +0800

    update bean作用域与生命周期

---
+++ Spring5/src/com/filwsx/bean/factoryBean/Order.java
@@ -0,0 +1,27 @@
+package com.filwsx.bean.factoryBean;
+
+/**
+ * @author filwsx
+ * @create 2022-04-02 12:50
+ */
+public class Order {
+    private String str;
+    public Order() {
+        System.out.println("空参构造器被调用了");
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+        System.out.println("set被调用了");
+    }
+
+    // 需要配置
+    public void initMethod(){
+        System.out.println("initMethod被调用了");
+    }
+
+    // 需要配置
+    public void destroyBean(){
+        System.out.println("destroyBean被调用了");
+    }
+}
--- Spring5/src/com/filwsx/test/BeanCollectionTypeTest.java
+++ Spring5/src/com/filwsx/test/BeanCollectionTypeTest.java
@@ -2,7 +2,9 @@ package com.filwsx.test;
 
 import com.filwsx.bean.collection.Cou;
 import com.filwsx.bean.collection.Stu;
+import com.filwsx.bean.factoryBean.Order;
 import com.filwsx.bean.school.Student;
+import com.filwsx.bean.school.Teacher;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -32,5 +34,28 @@ public class BeanCollectionTypeTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/filwsx/factoryBean1.xml");
         Student student = context.getBean("myBean", Student.class);
         System.out.println(student);
+    }
+
+    @Test
+    // 测试单实例与多实例
+    public void test4(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/filwsx/BeanXmlScope.xml");
+        Student student1 = context.getBean("student", Student.class);
+        Student student2 = context.getBean("student", Student.class);
+        Teacher teacher1 = context.getBean("teacher", Teacher.class);
+        Teacher teacher2 = context.getBean("teacher", Teacher.class);
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(teacher1);
+        System.out.println(teacher2);
+    }
+
+    @Test
+    // 测试Bean的生命周期
+    public void test5(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/filwsx/BeanXmlProcess.xml");
+        Order order = context.getBean("order", Order.class);
+        System.out.println("已经获取对象:" + order);
+        ((ClassPathXmlApplicationContext)context).close();
     }
 }
