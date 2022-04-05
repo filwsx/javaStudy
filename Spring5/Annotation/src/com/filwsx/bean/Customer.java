a21ed23ebd3ef3a228db0203a0ad96cb60649750
filwsx <endwoggg@gmail.com>
2022-04-05 13:31:07 +0800

    update beanAnnotation Succeed

--- Spring5/Annotation/src/com/filwsx/bean/Customer.java
+++ Spring5/Annotation/src/com/filwsx/bean/Customer.java
@@ -4,6 +4,6 @@ package com.filwsx.bean;
  * @author filwsx
  * @create 2022-04-05 12:50
  */
-public class customer {
+public class Customer {
 
 }
--- Spring5/Annotation/src/com/filwsx/service/CustomerService.java
+++ Spring5/Annotation/src/com/filwsx/service/CustomerService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
  * @create 2022-04-05 12:51
  */
 
-@Component()
-// @Component(value = "customerService")
+//@Component() 括号里不写东西，程序就一直在运行也不报错和输出，不知道为什么
+@Component(value = "customerService")
 public class CustomerService {
     public void add(){
         System.out.println("CustomerService add...");
