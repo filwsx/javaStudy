cba9c864b8842d2b30f6c4852601a32d47703e10
filwsx <endwoggg@gmail.com>
2022-04-10 11:41:14 +0800

    update AOP的几个通知的测试

--- Spring5/Annotation/src/com/filwsx/bean/CustomerDAOProxy.java
+++ Spring5/Annotation/src/com/filwsx/bean/CustomerDAOProxy.java
@@ -1,7 +1,7 @@
 package com.filwsx.bean;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,8 +13,51 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class CustomerDAOProxy {
 
-    @Before(value = "execution(* com.filwsx.bean.CustomerDAO.add(..))")
-    public void before(){
-        System.out.println("before......");
+//    @Before(value = "execution(* com.filwsx.bean.CustomerDAO.add(..))")
+//    public void before(){
+//        System.out.println("before......");
+//    }
+
+    //相同切入点抽取
+    @Pointcut(value = "execution(* com.filwsx.bean.CustomerDAO.add(..))")
+    public void pointdemo() {
+
     }
+
+    //前置通知
+    //@Before注解表示作为前置通知
+    @Before(value = "pointdemo()")
+    public void before() {
+        System.out.println("before.........");
+    }
+
+    //后置通知（返回通知）
+    @AfterReturning(value = "pointdemo()")
+    public void afterReturning() {
+        System.out.println("afterReturning.........");
+    }
+
+    //最终通知
+    @After(value = "pointdemo()")
+    public void after() {
+        System.out.println("after.........");
+    }
+
+    //异常通知
+    @AfterThrowing(value = "pointdemo()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing.........");
+    }
+
+    //环绕通知
+    @Around(value = "pointdemo()")
+    public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("环绕之前.........");
+
+        //被增强的方法执行
+        proceedingJoinPoint.proceed();
+
+        System.out.println("环绕之后.........");
+    }
+
 }
