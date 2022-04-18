461d02993bc515a0396521c3d82d740dae26b38d
filwsx <endwoggg@gmail.com>
2022-04-18 12:09:04 +0800

    springMVC helloWorld bug fix

--- jobStudy/SpringMVC/HelloWorld-module-web/src/main/java/com/filwsx/springmvc/controller/HelloController.java
+++ jobStudy/SpringMVC/HelloWorld-module-web/src/main/java/com/filwsx/springmvc/controller/HelloController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/")
     public String getIndex(){
         return "index";
     }
 
     @RequestMapping(value = "/index2")
-    public String getIndex(){
+    public String getIndex2(){
         return "index2";
     }
 }
