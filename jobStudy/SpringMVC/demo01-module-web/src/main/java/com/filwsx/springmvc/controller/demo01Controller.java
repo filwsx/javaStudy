2e645ff3b1037cdf902b9a441028e8c9153783c0
filwsx <endwoggg@gmail.com>
2022-04-19 14:20:56 +0800

    module 运行后404 是因为project structure设置问题，新建一个module即可正常

--- jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/demo01Controller.java
+++ jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/demo01Controller.java
@@ -1,18 +1,17 @@
-package com.filwsx.springmvc;
+package com.filwsx.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author filwsx
- * @create 2022-04-19 12:17
+ * @create 2022-04-19 13:31
  */
 @Controller
-public class frameworkController {
+public class demo01Controller {
 
     @RequestMapping("/")
     public String index(){
         return "index";
     }
-
 }
