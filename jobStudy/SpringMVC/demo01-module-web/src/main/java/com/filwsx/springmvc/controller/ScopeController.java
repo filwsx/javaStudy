b64dcbfeb4a4264faa4dac01a119f8dc70dd73a2
filwsx <endwoggg@gmail.com>
2022-05-05 11:09:51 +0800

    SpringMVC hello,ModelMap

--- jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/ScopeController.java
+++ jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/ScopeController.java
@@ -2,10 +2,12 @@ package com.filwsx.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author filwsx
@@ -30,6 +32,18 @@ public class ScopeController {
     @RequestMapping("/testModel")
     public String testModel(Model model){
         model.addAttribute("testScope","hello,Model");
+        return "success";
+    }
+
+    @RequestMapping("/testMap")
+    public String testMap(Map<String,Object> map){
+        map.put("testScope","hello,Map");
+        return "success";
+    }
+
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap){
+        modelMap.addAttribute("testScope","hello,ModelMap");
         return "success";
     }
 }
