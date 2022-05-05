c7ecab05eaa19d9ec632f4447a30634899721ec0
filwsx <endwoggg@gmail.com>
2022-05-05 11:38:58 +0800

    SpringMVC httpSession

--- jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/ScopeController.java
+++ jobStudy/SpringMVC/demo01-module-web/src/main/java/com/filwsx/springmvc/controller/ScopeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -32,18 +33,27 @@ public class ScopeController {
     @RequestMapping("/testModel")
     public String testModel(Model model){
         model.addAttribute("testScope","hello,Model");
+        System.out.println(model.getClass().getName());
         return "success";
     }
 
     @RequestMapping("/testMap")
     public String testMap(Map<String,Object> map){
         map.put("testScope","hello,Map");
+        System.out.println(map.getClass().getName());
         return "success";
     }
 
     @RequestMapping("/testModelMap")
     public String testModelMap(ModelMap modelMap){
         modelMap.addAttribute("testScope","hello,ModelMap");
+        System.out.println(modelMap.getClass().getName());
+        return "success";
+    }
+
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession httpSession){
+        httpSession.setAttribute("testScope","hello,httpSession");
         return "success";
     }
 }
