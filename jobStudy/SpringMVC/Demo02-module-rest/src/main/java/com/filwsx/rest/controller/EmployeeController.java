c22ef316369a00aea9908865fb0f617bff408dbd
filwsx <endwoggg@gmail.com>
2022-05-17 14:59:59 +0800

    update

--- jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
+++ jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.filwsx.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,5 +28,11 @@ public class EmployeeController {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("employees",employees);
         return "employees";
+    }
+
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
     }
 }
