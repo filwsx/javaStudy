572d1678e87b75d417b5c24c231740ff694abb81
filwsx <endwoggg@gmail.com>
2022-05-17 15:17:15 +0800

    update

--- jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
+++ jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
@@ -35,4 +35,24 @@ public class EmployeeController {
         employeeDao.delete(id);
         return "redirect:/employee";
     }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
 }
