6046c398d955a69d9be3d59943e827a00c92ccc4
filwsx <endwoggg@gmail.com>
2022-05-19 10:40:30 +0800

    update

--- jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
+++ jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/EmployeeController.java
@@ -5,7 +5,6 @@ import com.filwsx.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/HttpController.java
+++ jobStudy/SpringMVC/Demo02-module-rest/src/main/java/com/filwsx/rest/controller/HttpController.java
@@ -1,5 +1,51 @@
-package com.filwsx.rest.controller;/**
-    @author filwsx
-    @create 2022-05-19 8:30
-*/public class HttpController {
+package com.filwsx.rest.controller;
+
+import com.filwsx.rest.bean.Employee;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author filwsx
+ * @create 2022-05-19 8:30
+ */
+
+@Controller
+public class HttpController {
+
+    @RequestMapping("/testRequestBody")
+    public String testRequestBody(@RequestBody String requestBody){
+        System.out.println("requestBody：" + requestBody);
+        return "success";
+    }
+
+    @RequestMapping("/testRequestEntity")
+    public String testRequestEntity(RequestEntity<String> requestEntity){
+        System.out.println("requestHeader:"+requestEntity.getHeaders());
+        System.out.println("requestBody:"+requestEntity.getBody());
+        return "success";
+    }
+
+    @RequestMapping("/testResponse")
+    public void testResponse(HttpServletResponse response) throws IOException {
+        response.getWriter().print("hello,filwsx");
+    }
+
+    @RequestMapping(value = "/testResponseBody", produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String testResponseBody(){
+        return "成功";
+    }
+
+    @RequestMapping("/testResponseUser")
+    @ResponseBody
+    public Employee testResponseUser(){
+        return new Employee(1001, "admin", "123456@110.com", 0);
+    }
+
 }
