bf8c5ae8910bfc2203e6dfd9c0e55cc432483464
filwsx <endwoggg@gmail.com>
2022-05-21 15:08:41 +0800

    my first MyBatis test ,success

---
+++ jobStudy/MyBatis/demo1/src/main/java/com/filwsx/mybatis/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.filwsx.mybatis.mapper;
+
+import com.filwsx.mybatis.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author filwsx
+ * @create 2022-05-21 14:32
+ */
+
+public interface UserMapper {
+    /**
+     * MyBatis面向接口编程的两个一致：
+     * 1、映射文件的namespace要和mapper接口的全类名保持一致
+     * 2、映射文件中SQL语句的id要和mapper接口中的方法名一致
+     *
+     * 表--实体类--mapper接口--映射文件
+     */
+
+    /**
+     * 添加用户信息
+     */
+    int insertUser();
+}
+
---
+++ jobStudy/MyBatis/demo1/src/test/java/com/filwsx/mybatis/FirstMyBatisTest.java
@@ -0,0 +1,38 @@
+package com.filwsx.mybatis;
+
+import com.filwsx.mybatis.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author filwsx
+ * @create 2022-05-21 14:46
+ */
+public class FirstMyBatisTest {
+
+    @Test
+    public void testMyBatis() throws IOException {
+
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        int res = mapper.insertUser();
+
+        sqlSession.commit();
+
+        System.out.println("res:"+ res);
+    }
+}
