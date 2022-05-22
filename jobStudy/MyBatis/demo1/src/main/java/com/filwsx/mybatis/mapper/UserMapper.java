afa2892636c8387dd32e7c89b40bf85f998a2502
filwsx <endwoggg@gmail.com>
2022-05-22 13:19:54 +0800

    update MyBatis HelloWorld done

--- jobStudy/MyBatis/demo1/src/main/java/com/filwsx/mybatis/mapper/UserMapper.java
+++ jobStudy/MyBatis/demo1/src/main/java/com/filwsx/mybatis/mapper/UserMapper.java
@@ -22,5 +22,25 @@ public interface UserMapper {
      * 添加用户信息
      */
     int insertUser();
+
+    /**
+     * 修改用户信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 根据id查询用户信息
+     */
+    User getUserById();
+
+    /**
+     * 查询所有的用户信息
+     */
+    List<User> getAllUser();
 }
 
--- jobStudy/MyBatis/demo1/src/test/java/com/filwsx/mybatis/FirstMyBatisTest.java
+++ jobStudy/MyBatis/demo1/src/test/java/com/filwsx/mybatis/FirstMyBatisTest.java
@@ -1,6 +1,7 @@
 package com.filwsx.mybatis;
 
 import com.filwsx.mybatis.mapper.UserMapper;
+import com.filwsx.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -34,5 +35,28 @@ public class FirstMyBatisTest {
         // sqlSession.commit();
 
         System.out.println("res:"+ res);
+    }
+
+    @Test
+    public void testCRUD() throws IOException {
+
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        // mapper.updateUser();
+
+//        User user = mapper.getUserById();
+//        System.out.println(user);
+        for (User user : mapper.getAllUser()) {
+            System.out.println(user);
+        }
+
     }
 }
