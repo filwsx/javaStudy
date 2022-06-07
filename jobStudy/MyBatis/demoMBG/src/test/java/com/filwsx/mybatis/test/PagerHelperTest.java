836a33aaf542215befa8799d320eb63aed0fc375
filwsx <endwoggg@gmail.com>
2022-06-07 12:25:35 +0800

    mybaits pageHelper

--- jobStudy/MyBatis/demoMBG/src/test/java/com/filwsx/mybatis/test/PagerHelperTest.java
+++ jobStudy/MyBatis/demoMBG/src/test/java/com/filwsx/mybatis/test/PagerHelperTest.java
@@ -3,7 +3,9 @@ package com.filwsx.mybatis.test;
 import com.filwsx.mybatis.bean.Emp;
 import com.filwsx.mybatis.mapper.EmpMapper;
 import com.filwsx.mybatis.mapper.utils.SqlSessionUtils;
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
@@ -19,9 +21,11 @@ public class PagerHelperTest {
     public void testPagerHelper(){
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-        PageHelper.startPage(2, 4);
+        Page<Object> page = PageHelper.startPage(2, 4);
         List<Emp> list = mapper.selectByExample(null);
         list.forEach(emp -> System.out.println(emp));
-
+        System.out.println(page);
+        PageInfo<Emp> pageInfo = new PageInfo<>(list,3);
+        System.out.println(pageInfo);
     }
 }
